b469380471594e41aedcec678e0b98796d70c959
dorbivas <dorbi@mta.ac.il>
2024-07-20 11:41:28 -0700

    test

--- kafka-bitnami.sh
+++ kafka-bitnami.sh
@@ -2,6 +2,8 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 # helm install my-kafka bitnami/kafka --set replicaCount=3 --set zookeeper.enabled=true --set zookeeper.replicaCount=3 --set allowPlaintextListener=yes
+#helm install my-kafka bitnami/kafka --set replicaCount=3 --set zookeeper.replicaCount=3 --set global.storageClass=standard --set persistence.enabled=false
+
 
 helm install my-kafka bitnami/kafka -f /home/bivas/code/Yotpo/kafka-app/my-kafka-app/values.yaml
 
