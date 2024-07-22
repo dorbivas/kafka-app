d9ba4417d99f82d46a4f3ccdf9b73ce509c4e8a6
dorbivas <dorbi@mta.ac.il>
2024-07-22 13:29:00 -0700

    test3 kafka sasl

--- kafka-bitnami.sh
+++ kafka-bitnami.sh
@@ -1,10 +1,8 @@
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo update
-
-# helm install my-kafka bitnami/kafka --set replicaCount=3 --set zookeeper.enabled=true --set zookeeper.replicaCount=3 --set allowPlaintextListener=yes
-#helm install my-kafka bitnami/kafka --set replicaCount=3 --set zookeeper.replicaCount=3 --set global.storageClass=standard --set persistence.enabled=false
-
-helm upgrade --install my-kafka bitnami/kafka -f values.yaml
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm repo update
+helm delete my-kafka
+helm dependency update kafka-cluster-chart
+helm upgrade --install my-kafka bitnami/kafka -f kafka-cluster-chart/values.yaml
 
 
 
