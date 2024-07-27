1321b08151049942ab61c26ec0481912a1394d74
dorbivas <dorbi@mta.ac.il>
2024-07-27 02:52:15 -0700

    stracture fix

--- rebuild-app.sh
+++ rebuild-app.sh
@@ -1,18 +1,10 @@
 #helm repo add bitnami https://charts.bitnami.com/bitnami
 #helm repo update
 
-# helm install my-kafka bitnami/kafka --set replicaCount=3 --set zookeeper.enabled=true --set zookeeper.replicaCount=3 --set allowPlaintextListener=yes
-#helm install my-kafka bitnami/kafka --set replicaCount=3 --set zookeeper.replicaCount=3 --set global.storageClass=standard --set persistence.enabled=false
-
 helm delete my-kafka-app
-helm upgrade --install my-kafka bitnami/kafka -f my-kafka-app/values.yaml
-helm upgrade --install my-kafka-app my-kafka-app
+helm upgrade --install kafka bitnami/kafka --version 29.3.13 --values kafka-config.yaml
 echo "services:" 
 kubectl get svc my-kafka
-echo "ep:"
-kubectl get ep my-kafka
-sleep 5
-kubectl logs -f $(kubectl get pods -l app.kubernetes.io/name=kafka -o jsonpath="{.items[0].metadata.name}")
-
+helm upgrade --install my-kafka-app . 
 
 
