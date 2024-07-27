80b1c5012bef3054f346b9b9b9ee8900c1754b88
dorbivas <dorbi@mta.ac.il>
2024-07-27 02:26:11 -0700

    Working when restarting kafka cluster

--- sol/kafka/utils.sh
+++ sol/kafka/utils.sh
@@ -1,18 +1,9 @@
-
-kubectl apply -f kafka-cleanup-job.yaml
-kubectl apply -f role.yaml
+#helm repo add bitnami https://charts.bitnami.com/bitnami
+#helm repo update
 helm upgrade --install kafka bitnami/kafka --version 29.3.13 --values kafka-config.yaml
 
-/opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server kafka-controller-0.kafka-controller-headless.default.svc.cluster.local:9092 --delete-offsets --group breeds_group --topic breeds_topic:0,1 
-/opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server kafka-controller-0.kafka-controller-headless.default.svc.cluster.local:9092 --delete --group breeds_group 
-/opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group breeds_group 
-/opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --reset-offsets --group breeds_group --topic breeds_topic:0,1s
-
-
-
+#kafka shells 
 /opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --delete --group breeds_group 
 /opt/bitnami/kafka/bin/kafka-consumer-groups.sh --bootstrap-server localhost:9092 --reset-offsets --group breeds_group --topic breeds_topic:0
-
-
-{ "partitions": [ { "topic": "breed_topic", "partition": 0, "offset": 0 } ], "version": 1 }
+json: { "partitions": [ { "topic": "breed_topic", "partition": 0, "offset": 0 } ], "version": 1 }
 /opt/bitnami/kafka/bin/kafka-delete-records.sh --bootstrap-server localhost:9092 --offset-json-file j.json
